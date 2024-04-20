5191dbf4bdde09537607cdd5729cd7b3be5ecc82
Monospace-V <monospace.substitute@gmail.com>
2024-04-20 11:50:20 +0530

    Modify core grid definition, allow for returning of lines

--- core.py
+++ core.py
@@ -1,14 +1,28 @@
 class grid:
-    def init(__self__, size = 4):
-        __self__.size = size
+    def __init__(self, size = 4):
+        self.size = size
         rows = []
         for row in range(size):
             rows.append([0] * size)
-        __self__.rows = rows
+        self.rows = rows
         columns = []
         for col_pos in range(size):
             column = []
-            for row in __self__.rows:
+            for row in self.rows:
                 column.append(row[col_pos])
             columns.append(column)
-        __self__.columns = columns
+        self.columns = columns
+    def down(self): # Returns wrt bottom. Weigh toward start of list.
+        out=[]
+        for column in self.columns:
+            out.append(column[::-1])
+        return out
+    def up(self):
+        return self.columns
+    def left(self):
+        return self.rows
+    def right(self):
+        out=[]
+        for row in self.rows:
+            out.append(row[::-1])
+        return out
