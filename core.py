e0106cee073465d5a5ebfbe347e04ce51548435e
Monospace-V <monospace.substitute@gmail.com>
2024-04-20 12:20:59 +0530

    rephrase returning rows and columns

--- core.py
+++ core.py
@@ -5,24 +5,33 @@ class grid:
         for row in range(size):
             rows.append([0] * size)
         self.rows = rows
-        columns = []
-        for col_pos in range(size):
-            column = []
-            for row in self.rows:
-                column.append(row[col_pos])
-            columns.append(column)
-        self.columns = columns
-    def down(self): # Returns wrt bottom. Weigh toward start of list.
-        out=[]
+        self.columns = columnize(self.rows)
+    def vertical(self, reverse = False):
+        out = []
         for column in self.columns:
-            out.append(column[::-1])
+            if reverse: column = column[::-1]
+            out.append(column)
         return out
-    def up(self):
-        return self.columns
-    def left(self):
-        return self.rows
-    def right(self):
-        out=[]
+    def horizontal(self, reverse = False):
+        out = []
         for row in self.rows:
-            out.append(row[::-1])
+            if reverse: row = row[::-1]
+            out.append(row)
         return out
+    def update(self,rows):
+        self.rows = rows
+        self.columns = columnize(rows)
+
+def columnize(rows):
+    columns = []
+    for col_pos in range(size):
+        column = []
+        for row in rows:
+            column.append(row[col_pos])
+        columns.append(column)
+    return columns
+
+
+def swipe_down(grid):
+    columns = grid.vertical(reverse = True)
+    
